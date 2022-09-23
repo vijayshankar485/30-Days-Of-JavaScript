447fee162ff659a1b6b5e380144e9f3a59a4363b
Vijay Kummamuru <vijayshankar485@gmail.com>
2022-09-23 18:14:26 +0530

    solar system

--- 24_Day_Project_solar_system/24_day_starter/scripts/main.js
+++ 24_Day_Project_solar_system/24_day_starter/scripts/main.js
@@ -1,2 +1,55 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+
+document.getElementById("calweight").addEventListener('click',Weight)
+
+function Weight(){
+let Mass = document.getElementById("mass").value
+if(Mass=="" || isNaN(Mass)){
+    alert("Enter Mass To Calculate Weight")
+}
+var select = document.getElementById('planets')
+var option = select.options[select.selectedIndex]
+if(option.value == "none"){
+    alert("Select Planet")
+}
+if(option.value == "Mercury"){
+    const surfaceGravity = 0.38
+    Weight = Mass*surfaceGravity
+    document.getElementById("weight").innerHTML +=`your weight on ${option.value} is`+" "+ Weight+"kgs"
+    
+}
+if(option.value == "Venus"){
+    const surfaceGravity = 0.91
+    Weight = Mass*surfaceGravity
+    document.getElementById("weight").innerHTML +=`your weight on ${option.value} is`+" "+ Weight+"kgs"
+}
+if(option.value == "Earth"){
+    const surfaceGravity = 1.0
+    Weight = Mass*surfaceGravity
+    document.getElementById("weight").innerHTML +=`your weight on ${option.value} is`+" "+ Weight+"kgs"
+}
+if(option.value == "Mars"){
+    const surfaceGravity = 0.38
+    Weight = Mass*surfaceGravity
+    document.getElementById("weight").innerHTML +=`your weight on ${option.value} is`+" "+ Weight+"kgs"
+}
+if(option.value == "Jupiter"){
+    const surfaceGravity = 2.34	
+    Weight = Mass*surfaceGravity
+    document.getElementById("weight").innerHTML +=`your weight on ${option.value} is`+" "+ Weight+"kgs"
+}
+if(option.value == "Saturn"){
+    const surfaceGravity = 0.93
+    Weight = Mass*surfaceGravity
+    document.getElementById("weight").innerHTML +=`your weight on ${option.value} is`+" "+ Weight+"kgs"
+}
+if(option.value == "Neptune"){
+    const surfaceGravity = 0.92
+    Weight = Mass*surfaceGravity
+    document.getElementById("weight").innerHTML +=`your weight on ${option.value} is`+" "+ Weight+"kgs"
+}
+if(option.value == "Pluto"){
+    const surfaceGravity = 1.12
+    Weight = Mass*surfaceGravity
+    document.getElementById("weight").innerHTML +=`your weight on ${option.value} is`+" "+ Weight+"kgs"
+}
+}
